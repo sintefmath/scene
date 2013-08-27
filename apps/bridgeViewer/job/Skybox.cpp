ca29cbc3f400fbe7e476542d4d38f5e834b0b926
MrGaribaldi <erik.bjonnes@sintef.no>
2013-08-27 11:23:32 +0200

    Debug shader rendering correctly.

--- apps/bridgeViewer/job/Skybox.cpp
+++ apps/bridgeViewer/job/Skybox.cpp
@@ -19,23 +19,45 @@ const GLint skybox_position_location = 8;
 const GLint skybox_cube_map_location = 5;
 const GLint skybox_mvp_location = 7;
 const GLint skybox_proj_location = 6;
-    
+  
 const char* skybox_fs = "#version 330\n\
     \n#extension GL_ARB_explicit_uniform_location : require\n        \
     \n                                                \
     in vec3 texCoords;\n                              \
     \n                                                \
-    layout(location=5) uniform samplerCube skyboxCube;\n                 \
+//    layout(location=5) uniform samplerCube skyboxCube;\n                 \
     \n                                                \
     out vec4 colour;\n                                \
     \n                                                \
     void main(void)\n                                 \
     {\n                                               \
-    colour = texture( skyboxCube, texCoords );\n      \
+  //  colour = texture( skyboxCube, texCoords );\n      \
     colour.r = 1.0f;\n                            \
+    colour.g = 0.25f;\n                            \
+    colour.b = 1.0f;\n                            \
     colour.w = 1.0f;\n                                 \
     }\n";
-    
+
+    const char* skybox_vs = "\
+#version 330\n                                          \
+void main(void)\n                                       \
+{\n                                                     \
+  //drawing single triangle to cover entire screen\n    \
+  if( gl_VertexID == 0 ) {\n                            \
+    gl_Position = vec4( -1.0, -1.0, 0.0, 1.0 );\n       \
+  }\n                                                   \
+  else if( gl_VertexID == 1) {\n                        \
+    gl_Position = vec4( 1.0, -1.0, 0.0, 1.0 );\n        \
+  }\n                                                   \
+  else if( gl_VertexID == 2) {\n                        \
+    gl_Position = vec4( -1.0, 1.0, 0.0, 1.0 );\n        \
+  }else if( gl_VertexID == 3) {\n                       \
+    gl_Position = vec4( 1.0, 1.0, 0.0, 1.0 );\n         \
+  }\n                                                   \
+}\n                                                     \
+";
+
+    /*
 const char* skybox_vs = "#version 330\n\
     \n#extension GL_ARB_explicit_uniform_location : require\n    \
     \n                                                \
@@ -48,68 +70,67 @@ const char* skybox_vs = "#version 330\n\
     \n                                                \
     void main(void){\n                                              \
     vec3 wPos = ( modelView * vec4(inPosition.xyz, 0.0f) ).xyz;\n      \
-    gl_Position =  projection  * modelView * vec4(inPosition.xyz, 1.0 );\n       \
-//    gl_Position = vec4(inPosition.xyz, 1.0 );\n       \
+//gl_Position =  projection  * modelView * vec4(inPosition.xyz, 1.0 );\n       \
+    gl_Position = vec4(inPosition.xyz, 1.0 );\n       \
     \n                                                              \
     texCoords = wPos;\n                                       \
     }\n";
+    */    
+
     
-
-    /*
-const float skybox_vertices[18] = {
-    -100.0, -100.0, -100.0,
-    100.0, -100.0, -50.0,
-    100.0, 100.0, 100.0,
-
-    100.0, 100.0, 100.0,
-    -100.0, 100.0, -50.0,
-    -100.0, -100.0, -100.0
+const float skybox_vertices[16] = {
+    -1.0, -1.0, 0.0, 1.0,
+     1.0, -1.0, 0.0, 1.0,
+    -1.0,  1.0, 0.0, 1.0,
+     1.0,  1.0, 0.0, 1.0
+};
+    
+    
+    /**
+const float skybox_vertices[144] = { 
+    -1.0f, -1.0f, -1.0f, 0.0f, //front
+     1.0f, -1.0f, -1.0f, 0.0f,
+     1.0f,  1.0f, -1.0f, 0.0f,
+     1.0f,  1.0f, -1.0f, 0.0f,
+    -1.0f,  1.0f, -1.0f, 0.0f,
+    -1.0f, -1.0f, -1.0f, 0.0f,
+                        
+    -1.0f, -1.0f, 1.0f, 0.0f, //back
+     1.0f, -1.0f, 1.0f, 0.0f,
+     1.0f,  1.0f, 1.0f, 0.0f,
+     1.0f,  1.0f, 1.0f, 0.0f,
+    -1.0f,  1.0f, 1.0f, 0.0f,
+    -1.0f, -1.0f, 1.0f, 0.0f,
+                        
+    -1.0f, -1.0f, -1.0f, 0.0f, //bottom
+     1.0f, -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f, -1.0f, 0.0f,
+                        
+    -1.0f, 1.0f, -1.0f, 0.0f, //top
+     1.0f, 1.0f, -1.0f, 0.0f,
+     1.0f, 1.0f,  1.0f, 0.0f,
+     1.0f, 1.0f,  1.0f, 0.0f,
+    -1.0f, 1.0f,  1.0f, 0.0f,
+    -1.0f, 1.0f, -1.0f, 0.0f,
+                        
+    -1.0f, -1.0f, -1.0f, 0.0f, //left
+    -1.0f,  1.0f, -1.0f, 0.0f,
+    -1.0f,  1.0f,  1.0f, 0.0f,
+    -1.0f,  1.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f,  1.0f, 0.0f,
+    -1.0f, -1.0f, -1.0f, 0.0f,
+                        
+     1.0f, -1.0f, -1.0f, 0.0f, //right
+     1.0f,  1.0f, -1.0f, 0.0f,
+     1.0f,  1.0f,  1.0f, 0.0f,
+     1.0f,  1.0f,  1.0f, 0.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+     1.0f, -1.0f, -1.0f, 0.0f                                            
 };
     */
-
-const float skybox_vertices[144] = { -1.0f, -1.0f, -1.0f, 0.0f, //front
-    1.0f, -1.0f, -1.0f, 0.0f,
-    1.0f, 1.0f, -1.0f, 0.0f,
-    1.0f, 1.0f, -1.0f, 0.0f,
-    -1.0f, 1.0f, -1.0f, 0.0f,
-    -1.0f, -1.0f, -1.0f, 0.0f,
-
-    -1.0f, -1.0f, 1.0f, 0.0f, //back
-    1.0f, -1.0f, 1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, -1.0f, 1.0f, 0.0f,
-
-    -1.0f, -1.0f, -1.0f, 0.0f, //bottom
-    1.0f, -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 1.0f, 0.0f,
-    1.0f, -1.0f, 1.0f, 0.0f,
-    -1.0f, -1.0f, 1.0f, 0.0f,
-    -1.0f, -1.0f, -1.0f, 0.0f,
-
-    -1.0f, 1.0f, -1.0f, 0.0f, //top
-    1.0f, 1.0f, -1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, 1.0f, -1.0f, 0.0f,
-
-    -1.0f, -1.0f, -1.0f, 0.0f, //left
-    -1.0f, 1.0f, -1.0f, 0.0f,
-    -1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, 1.0f, 1.0f, 0.0f,
-    -1.0f, -1.0f, 1.0f, 0.0f,
-    -1.0f, -1.0f, -1.0f, 0.0f,
-           
-    1.0f, -1.0f, -1.0f, 0.0f, //right
-    1.0f, 1.0f, -1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    1.0f, 1.0f, 1.0f, 0.0f,
-    1.0f, -1.0f, 1.0f, 0.0f,
-    1.0f, -1.0f, -1.0f, 0.0f                                            
-};
-
 
 
 bool checkShader( GLuint shaderID )
@@ -260,18 +281,19 @@ Skybox::init()
 
     //setup vao
     glGenVertexArrays( 1, &m_vao );
-    glBindVertexArray( m_vao );
-    GLuint vbo;
-    glGenBuffers( 1, &vbo );
-    glBindBuffer( GL_ARRAY_BUFFER, vbo );
-    glBufferData( GL_ARRAY_BUFFER, sizeof(float) * 144, skybox_vertices, GL_STATIC_DRAW );
-    glVertexAttribPointer( skybox_position_location, 4, GL_FLOAT, false, 0, nullptr );
-    glBindBuffer( GL_ARRAY_BUFFER, 0 );
+//    glBindVertexArray( m_vao );
+//    GLuint vbo;
+//    glGenBuffers( 1, &vbo );
+//    glBindBuffer( GL_ARRAY_BUFFER, vbo );
+//    glBufferData( GL_ARRAY_BUFFER, sizeof(float) * 15, skybox_vertices, GL_STATIC_DRAW );
+//    glVertexAttribPointer( skybox_position_location, 4, GL_FLOAT, false, 0, nullptr );
+//    glEnableVertexAttribArray( 0 );
+//    glBindBuffer( GL_ARRAY_BUFFER, 0 );
     glBindVertexArray( 0 );
 
 
     //setup cubemap
-    createCubeMap();
+    //    createCubeMap();
     CHECK_GL;
     m_shader_prog = createSkyboxShader();
     CHECK_GL;
@@ -282,58 +304,58 @@ Skybox::render( float* mvp, float* p )
 {
     glUseProgram( m_shader_prog );
     glBindVertexArray( m_vao );
-
-    glActiveTexture( GL_TEXTURE0 );
-    glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
+//    glActiveTexture( GL_TEXTURE0 );
+//    glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
     CHECK_GL;
-    glUniform1i( skybox_cube_map_location, 0 );
-    glUniformMatrix4fv( skybox_mvp_location, 1, GL_FALSE, mvp );
-    glUniformMatrix4fv( skybox_proj_location, 1, GL_FALSE, p );
+    //    glUniform1i( skybox_cube_map_location, 0 );
+    //glUniformMatrix4fv( skybox_mvp_location, 1, GL_FALSE, mvp );
+    //glUniformMatrix4fv( skybox_proj_location, 1, GL_FALSE, p );
     CHECK_GL;
-    
-    glDrawArrays( GL_TRIANGLES, 0, 6 );
+    //glDrawArrays( GL_TRIANGLES, 0, 6 );
+    glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
     glBindBuffer( GL_ARRAY_BUFFER, 0 );
     CHECK_GL;
-    glActiveTexture( GL_TEXTURE0 );
-    glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
+    //    glActiveTexture( GL_TEXTURE0 );
+    //    glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
     glUseProgram( 0 );
     glBindVertexArray( 0 );
     CHECK_GL;
+
 }
 
 void
 Skybox::createCubeMap( )
 {
-    glGenTextures( 1, &m_cubemap);
-    glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
-    //    glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
-    //hardcoding location of skybox images for now.
-    //would be nice to just embed them in .exe or something, but will not add qtResource for it now.
-    //will need image loading lib for this, using stb_image  
-
-    //names need to be skybox_[side].png, with side= [east, west, up, down, south, north]
-    //ordering corresponding to GL_TEXTURE_CUBE_MAP_[side] ordering.
-    int x,y,n;
-    std::vector< std::string > img_names;
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_east.jpg" );
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_west.jpg" );
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_up.jpg" );
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_down.jpg" );
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_south.jpg" );
-    img_names.push_back( std::string(m_tex_base_path) + "skybox_north.jpg" );
-
-    unsigned char* temp;
-    int format;
-    for(auto i = 0u; i < img_names.size(); ++i )
-    {
-        temp = stbi_load( img_names[i].c_str(), &x, &y, &n, 4 );
-        format = GL_RGBA; //n == 4 ? GL_RGBA : GL_RGB;
-        glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, x, y, 0, GL_RGBA, GL_BYTE, (void*)temp );
-        //        glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, x, y, 0, GL_RGBA, GL_BYTE, (void*)temp );
-
-    }
-
-    glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
+    //glGenTextures( 1, &m_cubemap);
+    //glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
+    ////    glBindTexture( GL_TEXTURE_CUBE_MAP, m_cubemap );
+    ////hardcoding location of skybox images for now.
+    ////would be nice to just embed them in .exe or something, but will not add qtResource for it now.
+    ////will need image loading lib for this, using stb_image  
+	//
+    ////names need to be skybox_[side].png, with side= [east, west, up, down, south, north]
+    ////ordering corresponding to GL_TEXTURE_CUBE_MAP_[side] ordering.
+    //int x,y,n;
+    //std::vector< std::string > img_names;
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_east.jpg" );
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_west.jpg" );
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_up.jpg" );
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_down.jpg" );
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_south.jpg" );
+    //img_names.push_back( std::string(m_tex_base_path) + "skybox_north.jpg" );
+	//
+    //unsigned char* temp;
+    //int format;
+    //for(auto i = 0u; i < img_names.size(); ++i )
+    //{
+    //    temp = stbi_load( img_names[i].c_str(), &x, &y, &n, 4 );
+    //    format = GL_RGBA; //n == 4 ? GL_RGBA : GL_RGB;
+    //    glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, x, y, 0, GL_RGBA, GL_BYTE, (void*)temp );
+    //    //        glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, x, y, 0, GL_RGBA, GL_BYTE, (void*)temp );
+	//
+    //}
+	//
+    //glBindTexture( GL_TEXTURE_CUBE_MAP, 0 );
 
 }
 
