63e846601b0f47178107aeff11602d6eb054c39f
Erik W. Bjoennes <erik.bjonnes@sintef.no>
2013-09-02 10:43:22 +0200

    Now loads files sent as input arguments, or bridgeData/chemco.dae if no file is specified.

--- apps/bridgeViewer/main_cloud.cpp
+++ apps/bridgeViewer/main_cloud.cpp
@@ -11,9 +11,15 @@ main( int argc, char** argv )
     Scene::initLogger( &argc, argv );
 
     std::list<std::string> files;
-    std::cerr << "adding /usr/var/trell/apps/bridgeData/chemco.dae" << std::endl;
-    files.push_back("/usr/var/trell/apps/bridgeData/chemco.dae");
-    std::cerr << "added /usr/var/trell/apps/bridgeData/chemco.dae, will now load it" << std::endl;
+    if(argc > 1){
+    for(int i=1; i<argc; i++ ) {
+      files.push_back( argv[i] );
+    }
+    }else{
+      std::cerr << "adding /usr/var/trell/apps/bridgeData/chemco.dae" << std::endl;
+      files.push_back("/usr/var/trell/apps/bridgeData/chemco.dae");
+      std::cerr << "added /usr/var/trell/apps/bridgeData/chemco.dae, will now load it" << std::endl;
+    }
     TiniaViewerJob job( files );
     tinia::trell::IPCGLJobController controller;
     controller.setJob( &job );
