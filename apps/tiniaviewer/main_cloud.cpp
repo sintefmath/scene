0be815875e51e9304fc321d6d5325993fe000143
Jens Olav Nygaard <Jens.O.Nygaard@sintef.no>
2014-06-27 13:59:45 +0200

    Small fix

--- apps/tiniaviewer/main_cloud.cpp
+++ apps/tiniaviewer/main_cloud.cpp
@@ -29,12 +29,12 @@ main( int argc, char** argv )
     Scene::initLogger( &argc, argv );
 
     std::list<std::string> files;
-//    for(int i=1; i<argc; i++ ) {
-//        files.push_back( argv[i] );
-//    }
-    std::cerr << "adding /tmp/bridgeTest.dae" << std::endl;
-    files.push_back("/tmp/bridgeTest.dae");
-    std::cerr << "added /tmp/bridgeTest.dae, will now load it" << std::endl;
+    for(int i=1; i<argc; i++ ) {
+        files.push_back( argv[i] );
+    }
+//     std::cerr << "adding /tmp/bridgeTest.dae" << std::endl;
+//     files.push_back("/tmp/bridgeTest.dae");
+//     std::cerr << "added /tmp/bridgeTest.dae, will now load it" << std::endl;
     TiniaViewerJob job( files );
     std::cerr << "has added files to TiniaViewer job, something should happen"<<std::endl;
     tinia::trell::IPCGLJobController controller;
